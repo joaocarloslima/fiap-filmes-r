357300fc46ea6974717ed71e16cb7efd550c3711
Joao <joaocarloslima@me.com>
2023-08-20 10:11:19 -0300

    projeto

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return ( //JSX
+    <>
+      <nav className="bg-slate-900 p-4">
+        <h1 className="text-3xl font-bold">Fiap Filmes</h1>
+      </nav>
+
+    </>
+  )
+}
